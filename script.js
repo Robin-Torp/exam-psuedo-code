337ecdac85d5c8638871115e2cf7d7a9c992a755
Robin-Torp <3kdyhfe24.torpro@folkuniversitetet.nu>
2024-12-19 22:21:45 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,47 @@
+// G-Version - Split the Nota
+// Skriv din pseudokod innanför nedanstående kommentarsblock
+/*
+Start
+    1. Ange:
+        total_belopp
+        antal_personer
+        dricks_procent
+    
+    2. Beräkna dricks:
+        dricks_belopp = total_belopp * dricks_procent / 100
+    
+    3. Beräkna summa att betala:
+        total_summa = total_belopp + dricks belopp
+    
+    4. Beräkna belopp per person:
+        belopp_per_person = total_summa / antal_personer
+    
+    5. Visa resultat:
+        Print: "Belopp per person: " + belopp_per_person
+
+*/
+
+// VG-Version - Lewis Carroll Word Puzzle
+// Skriv din pseudokod innanför nedanstående kommentarsblock
+// Startkoden är 100% frivillig och kan tas bort eller skrivas om
+/*
+
+play();
+
+function play()
+    SET variabel ordbok = [FOUR, FOUL, FOOL, FOOT, FORT, FORE, FIRE, FIVE]; // Innehåller ALLA ord i det engelska språket
+    SET variabel startOrd till "FOUR";
+    SET variabel slutordOrd till "FIVE";
+
+
+end function
+
+function isOneLetterApart(wordOne, wordTwo)
+    SET variabel diffCount till 0;
+
+    // Här behöver du skriva koden för din funktion
+
+    return diffCount === 1; // returnerar sant om endast en bokstav ändrats, annars falskt
+end function
+
+*/
